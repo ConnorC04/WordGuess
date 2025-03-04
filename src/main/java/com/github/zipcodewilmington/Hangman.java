02eb005c90ebfc0ee1baf5d2a6b3277d77fca909
ConnorC04 <connorjcasey@iCloud.com>
2025-03-04 16:58:23 -0500

    its 5 o'clock somewhere

--- src/main/java/com/github/zipcodewilmington/Hangman.java
+++ src/main/java/com/github/zipcodewilmington/Hangman.java
@@ -4,9 +4,6 @@ package com.github.zipcodewilmington;
 
 public class Hangman {
 
-
-
-
     public static void main(String[] args) {
         Wordguess game = new Wordguess();
         game.runGame();
--- src/main/java/com/github/zipcodewilmington/Wordguess.java
+++ src/main/java/com/github/zipcodewilmington/Wordguess.java
@@ -1,73 +1,107 @@
 package com.github.zipcodewilmington;
 
-import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Wordguess {
 
     String[] wordBank = {"cat", "dog", "laptop", "zipcode", "spider-man"};
 
 
-    public void runGame(){
+    public void runGame() {
+
+        Scanner scan = new Scanner(System.in);
+        String userInput = scan.nextLine();
 
         System.out.println("This is the game running");
-        String wordToGuess = randWord(wordBank);
-        int guessesLeft = setGuess(wordToGuess);
-        char[] playerArr = setPlayerArr(wordToGuess);
-        char[] gameArr = setGameArr(wordToGuess);
-
-        System.out.println(arrToStringFormat(playerArr));
-        System.out.println(arrToStringFormat(gameArr));
-
     }
 
 
     // This method is passed a string array and returns a string
 
-    public static String randWord(String[] arr){
+    public static String randWord(String[] arr) {
         int num = (int) (Math.random() * arr.length);
         return arr[num];
     }
 
-    public static int setGuess(String s){
+    public static int setGuess(String s) {
         return s.length(); // integer used to set the guesses the player has
     }
 
-    public static char[] setPlayerArr(String s){
+    public static char[] setPlayerArr(String s) {
 
         // makes new charArr equal to length of randWord
         char[] playerArr = new char[setGuess(s)];
-        for (int i = 0; i < setGuess(s); i++){
+        for (int i = 0; i < setGuess(s); i++) {
             playerArr[i] = '_';
         }
-//        for (int i = 0; i < setGuess(s) * 2; i++){ // iterates through each index of the new array
-//            if ((i) % 2 == 0){ // if the index is even
-//                playerArr[i] = '_'; // replace the index of the charArr with the charAt the index of s
-//            }else{ // if the index is odd
-//                playerArr[i] = ' '; // replace the index of the charArr with ' '
-//            }
-//        }
+
         return playerArr;
     }
 
-    public static char[] setGameArr(String s){
+    public static char[] setGameArr(String s) {
         // makes new charArr equal to length of randWord
         char[] gameArr = new char[setGuess(s)];
-        for (int i = 0; i < setGuess(s); i++){
+        for (int i = 0; i < setGuess(s); i++) {
             gameArr[i] = s.charAt(i);
         }
-//        for (int i = 0; i < setGuess(s) * 2; i++){ // iterates through each index of the new array
-//            if ((i) % 2 == 0){ // if the index is even
-//                gameArr[i] = s.charAt(i); // replace the index of the charArr with the charAt the index of s
-//            }else{ // if the index is odd
-//                gameArr[i] = ' '; // replace the index of the charArr with ' '
-//            }
-//        }
+
         return gameArr;
     }
 
-    public static String arrToStringFormat(char[] cArr){
+    public static String formatArray(char[] cArr) {
         return new String(cArr).replaceAll("", " ").trim();
     }
+
+    public static boolean didPlayerQuit(String s) {
+        return s.equals("-");
+    }
+
+    public static void playerQuitMessage() {
+        System.out.println("Thank you for playing, please come again soon!");
+    }
+
+    public static char processInput(String s) {
+
+
+        char c = ' '; // come back to this
+
+        if (didPlayerQuit(s)) {
+            playerQuitMessage();
+        } else {
+            for (int i : setGameArr(s)) {
+                String arrToString = String.valueOf((setGameArr(s))[i]);
+
+            }
+        }
+        return c;
+    }
+
+    public static char[] updatePlayerArray(char c){
+
+
+        return null;
+    }
+
+
+
+
+
+
+
+
+//          String[] noSpaces = phrase.split(" "); // creates string array with each word in the original string
+//          int wordCount = noSpaces.length; // Creates integer equal to the number of words in the string array
+//
+//
+//          char firstLetter;
+//          String wordBuilder = "";
+//          for (int i = 0; i < wordCount; i++){
+//              String arrToString = noSpaces[i];
+//              firstLetter = arrToString.charAt(0);
+//              wordBuilder = wordBuilder.concat(String.valueOf(firstLetter)).toUpperCase() ;
+//          }
+//          return wordBuilder;
 }
 
 
